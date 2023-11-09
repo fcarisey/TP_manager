fc3a4596c97abf31906511567ce9af0dc7bbce77
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2023-11-09 16:53:41 +0100

    user create form

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,17 +2,53 @@
 
 namespace App\Controller;
 
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateurController extends AbstractController
 {
     #[Route('/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(UtilisateurType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $utilisateur = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($utilisateur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_utilisateur');
+        }
+
         return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/utilisateur/search', name: 'app_utilisateur_search')]
+    public function search(Request $request): string
+    {
+        $s = $request->query->get('s');
+
+        if (is_null($s))
+            return json_encode(['error' => 'Paramètre de recherche manquant']);
+
+        $s_exploded = explode(' ', $s);
+
+//        // find user from nom, prenom
+//        (new UtilisateurRepository)->findBy()
+//        $users = $this->getDoctrine()->getRepository('App:Utilisateur')->findByNomPrenom($s_exploded);
+//        if (count($users) > 0)
+//            return json_encode($users);
+//
+//        return json_encode(['error' => 'Aucun utilisateur trouvé']);
+
+
     }
 }
--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -17,7 +17,6 @@ class UtilisateurType extends AbstractType
     {
         $builder
             ->add('nom')
-
             ->add('prenom')
             ->add('motDePasse', PasswordType::class)
             ->add('courriel', EmailType::class)
@@ -26,9 +25,7 @@ class UtilisateurType extends AbstractType
                 'class' => Classe::class,
                 'choice_label' => 'designation',
                 'required' => false
-            ])
-            ->add('taches') // À revoir avec l'état de la complétion de la tache
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
