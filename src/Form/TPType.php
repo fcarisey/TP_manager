1b3f758828014efbd47e73d348fc145f7b1ef957
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2023-12-05 16:12:28 +0100

    Fix TPType classe_id to classe

--- src/Form/TPType.php
+++ src/Form/TPType.php
@@ -4,12 +4,11 @@ namespace App\Form;
 
 use App\Entity\Classe;
 use App\Entity\TP;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TPType extends AbstractType
@@ -19,7 +18,7 @@ class TPType extends AbstractType
         $builder
             ->add('titre')
             ->add('description')
-            ->add('fichier', TextType::class, [
+            ->add('fichier', FileType::class, [
                 'required' => false
             ])
             ->add('date_debut', DateTimeType::class, [
@@ -30,7 +29,7 @@ class TPType extends AbstractType
                 'required' => false,
                 'widget' => 'single_text'
             ])
-            ->add('classe_id', EntityType::class, [
+            ->add('classe', EntityType::class, [
                 'class' => Classe::class,
                 'choice_label' => 'designation',
                 'required' => true
